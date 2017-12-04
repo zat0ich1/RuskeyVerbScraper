9e00d9f85b3253842ae7646d99de3d3e7a62ca24
eli <eli.ginsburg.marcy@gmail.com>
2017-12-03 22:04:34 -0600

    Cleaned verb data for first 50; started PollyPully.py to grab AWS audio

--- PollyPull.py
+++ PollyPull.py
@@ -1,6 +1,15 @@
 import os
 import requests
 import boto3
+import awskeys
+from contextlib import closing
+# awskeys is a python file containing two variable assignments:
+#keyid is assigned to the access key id passed to the  aws_access_key_id
+#param in the synthesize_speech call; secretkey is the aws secret key
+#passed to the aws_secret_access_key parameter;
+promptOne = "Send example above to AWS? Type enter or y to continue or n to cancel: "
+promptTwo = "Male or female voice for this exapmle? Type m or enter for male, f for female, or c to cancel "
+count = 0
 fileList = os.listdir('./verbs')
 fileList.sort()
 for file in fileList:
@@ -14,8 +23,31 @@ for file in fileList:
         verbFormsString += '</speak>'
         verbFormsString = verbFormsString.replace(u'\n','')
         verbExamplesList = []
-        for i in range(16,len(verbFileLinesList),2):
+        for i in range(16,len(verbFileLinesList)):
             verbExamplesList.append(verbFileLinesList[i].replace(u'\n',''))
-        print(verbFormsString)
-        for i in verbExamplesList:
-            print(i)
+        print("sending " + verbFormsString + " to AWS Polly")
+        #add code to create mp3 from verbFormsString
+        for i in range(0,len(verbExamplesList)-1,2):
+            print ('=' * 40)
+            print (verbExamplesList[i])
+            print (verbExamplesList[i+1])
+            sendToAWS = 'n'
+            count += 1
+#            sendToAWS = input(promptOne)
+#            while sendToAWS != 'y' and sendToAWS != 'n' and sendToAWS != '':
+#                print ("invalid selection")
+#                sendToAWS = input(promptOne)
+            if sendToAWS == 'n':
+                continue
+            elif sendToAWS == 'y' or sendToAWS == '':
+                voice = input (promptTwo)
+                while voice != 'm' and voice != 'f' and voice != 'c' and voice != '':
+                    print ('invalid selection')
+                    voice = input (promptTwo)
+                if voice == 'm' or voice == '':
+                    print("aws call with male voice")
+                elif voice == 'f':
+                    print("aws call with female voice")
+                elif voice == 'c':
+                    continue
+print(count)
