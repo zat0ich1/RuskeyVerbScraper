f0519dc47aacb35448434cc6c8ac252dfa3ec921
eli <eli.ginsburg.marcy@gmail.com>
2017-12-04 21:56:27 -0600

    created function for aws api calls to write mp3s to file

--- PollyPull.py
+++ PollyPull.py
@@ -1,42 +1,55 @@
 import os
 import requests
 import boto3
-import awskeys
+from awskeys import *
+
 from contextlib import closing
 # awskeys is a python file containing two variable assignments:
 #keyid is assigned to the access key id passed to the  aws_access_key_id
 #param in the synthesize_speech call; secretkey is the aws secret key
 #passed to the aws_secret_access_key parameter;
 promptOne = "Send example above to AWS? Type enter or y to continue or n to cancel: "
-promptTwo = "Male or female voice for this exapmle? Type m or enter for male, f for female, or c to cancel "
-count = 0
+promptTwo = "Male or female voice for this exapmle? Type f or enter for female, m for male, or c to cancel "
 fileList = os.listdir('./verbs')
 fileList.sort()
+print(os.getcwd)
+client = boto3.client('polly', region_name='us-west-2', aws_access_key_id=keyid, aws_secret_access_key=secretkey)
+
+def getAudio(fileName, text, textType='text', voiceID):
+    """makes an amazon polly api call to save an mp3 file of tts for text in voiceID's voice;
+        fileName - a string for the filename (e.g. 'file1.mp3');
+        text - a string of the text you want turned into audio (with or without ssml tags - see next param);
+        textType - 'ssml' or 'text' (default);
+        voiceID - a string of the voice name you wish to use; for Russian either 'Tatyana' or 'Maxim'"""
+    response = client.synthesize_speech(OutputFormat='mp3',Text=text, TextType=textType, VoiceId=voiceID)
+    with open(fileName, 'bw') as audiofile:
+        audiofile.write(response['AudioStream'].read())
+
 for file in fileList:
-    with open('./verbs/'+ file, 'r') as verbFile:
-        verbFileLinesList = verbFile.readlines()
-        verbFormsString = "<speak>"+verbFileLinesList[0] + '<break time="0.5s"/>'
+    audioFileName = file[-4] + '.mp3' #grabs text file name and replace .txt with .mp3
+    with open('./verbs/'+ file, 'r') as verbFile: #open each file in the ./verbs directory
+        verbFileLinesList = verbFile.readlines() #create a list containing each line of the file as an element
+        verbFormsString = "<speak>"+verbFileLinesList[0] + '<break time="0.5s"/>'#add a ssml .5 second break after the infinitive
         for i in range(4,16):
             verbFormsString += verbFileLinesList[i]
             if i != 15:
-                verbFormsString += '<break time="0.5s"/>'
+                verbFormsString += '<break time="0.5s"/>' #add a .5 second ssml break after the remaining conjugated forms (except the last one)
         verbFormsString += '</speak>'
-        verbFormsString = verbFormsString.replace(u'\n','')
+        verbFormsString = verbFormsString.replace(u'\n','') #remove the newline characters throughout
         verbExamplesList = []
         for i in range(16,len(verbFileLinesList)):
-            verbExamplesList.append(verbFileLinesList[i].replace(u'\n',''))
+            verbExamplesList.append(verbFileLinesList[i].replace(u'\n','')) #adds each example and translation to the list without newlines
         print("sending " + verbFormsString + " to AWS Polly")
-        #add code to create mp3 from verbFormsString
+        getAudio(fileName=audioFileName,text=verbFormsString,textType='ssml',voiceID='Tatyana')
         for i in range(0,len(verbExamplesList)-1,2):
             print ('=' * 40)
             print (verbExamplesList[i])
             print (verbExamplesList[i+1])
-            sendToAWS = 'n'
-            count += 1
-#            sendToAWS = input(promptOne)
-#            while sendToAWS != 'y' and sendToAWS != 'n' and sendToAWS != '':
-#                print ("invalid selection")
-#                sendToAWS = input(promptOne)
+            exampleAudioFileName = audioFileName[:-4] + str(i//2) + audioFileName[-4:] #add example number to file name
+            sendToAWS = input(promptOne)
+            while sendToAWS != 'y' and sendToAWS != 'n' and sendToAWS != '':
+                print ("invalid selection")
+                sendToAWS = input(promptOne)
             if sendToAWS == 'n':
                 continue
             elif sendToAWS == 'y' or sendToAWS == '':
@@ -44,10 +57,9 @@ for file in fileList:
                 while voice != 'm' and voice != 'f' and voice != 'c' and voice != '':
                     print ('invalid selection')
                     voice = input (promptTwo)
-                if voice == 'm' or voice == '':
-                    print("aws call with male voice")
-                elif voice == 'f':
-                    print("aws call with female voice")
+                if voice == 'm':
+                    getAudio(fileName=exampleAudioFileName, text=verbExamplesList[i], voiceID='Maxim')
+                elif voice == 'f' or voice == '':
+                    getAudio(fileName=exampleAudioFileName, text=verbExamplesList[i], voiceID='Tatyana')
                 elif voice == 'c':
                     continue
-print(count)
