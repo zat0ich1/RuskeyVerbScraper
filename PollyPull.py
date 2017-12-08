ccb5375e480a34ee5d5b7e6ac914864a046761e3
eli <eli.ginsburg.marcy@gmail.com>
2017-12-07 22:23:12 -0600

    additional data cleanup; fixed slicing error; reduced voice gender choice to sentences with I

--- PollyPull.py
+++ PollyPull.py
@@ -8,12 +8,19 @@ from contextlib import closing
 #keyid is assigned to the access key id passed to the  aws_access_key_id
 #param in the synthesize_speech call; secretkey is the aws secret key
 #passed to the aws_secret_access_key parameter;
-promptOne = "Send example above to AWS? Type enter or y to continue or n to cancel: "
-promptTwo = "Male or female voice for this exapmle? Type f or enter for female, m for male, or c to cancel "
+prompt = "Male or female voice for this exapmle? Type f or enter for female, m for male: "
 fileList = os.listdir('./verbs')
 fileList.sort()
 print(os.getcwd)
 client = boto3.client('polly', region_name='us-west-2', aws_access_key_id=keyid, aws_secret_access_key=secretkey)
+
+def containsI(sentence):
+    """returns True if a sentence contains the Russian pronoun я; false otherwise"""
+    sentenceList = sentence.split(' ')
+    if 'я' in sentenceList or 'Я' in sentenceList:
+        return True
+    else:
+        return False
 
 def getAudio(fileNameStr, translateString, voiceName, mode='text'):
     """makes an amazon polly api call to save an mp3 file of tts for text in voiceID's voice;
@@ -26,7 +33,7 @@ def getAudio(fileNameStr, translateString, voiceName, mode='text'):
         audiofile.write(response['AudioStream'].read())
 
 for file in fileList:
-    audioFileName = file[-4] + '.mp3' #grabs text file name and replace .txt with .mp3
+    audioFileName = './verbs/' + file[:-4] + '.mp3' #grabs text file name and replace .txt with .mp3
     with open('./verbs/'+ file, 'r') as verbFile: #open each file in the ./verbs directory
         verbFileLinesList = verbFile.readlines() #create a list containing each line of the file as an element
         verbFormsString = "<speak>"+verbFileLinesList[0] + '<break time="0.5s"/>'#add a ssml .5 second break after the infinitive
@@ -41,25 +48,20 @@ for file in fileList:
             verbExamplesList.append(verbFileLinesList[i].replace(u'\n','')) #adds each example and translation to the list without newlines
         print("sending " + verbFormsString + " to AWS Polly")
         getAudio(fileNameStr=audioFileName, translateString=verbFormsString, voiceName='Tatyana',mode='ssml')
+        print('getting',verbFormsString,'as',audioFileName)
         for i in range(0,len(verbExamplesList)-1,2):
             print ('=' * 40)
             print (verbExamplesList[i])
             print (verbExamplesList[i+1])
             exampleAudioFileName = audioFileName[:-4] + str(i//2) + audioFileName[-4:] #add example number to file name
-            sendToAWS = input(promptOne)
-            while sendToAWS != 'y' and sendToAWS != 'n' and sendToAWS != '':
-                print ("invalid selection")
-                sendToAWS = input(promptOne)
-            if sendToAWS == 'n':
-                continue
-            elif sendToAWS == 'y' or sendToAWS == '':
-                voice = input (promptTwo)
-                while voice != 'm' and voice != 'f' and voice != 'c' and voice != '':
+            if containsI(verbExamplesList[i]):
+                voice = input (prompt)
+                while voice != 'm' and voice != 'f' and voice != '':
                     print ('invalid selection')
-                    voice = input (promptTwo)
+                    voice = input (prompt)
                 if voice == 'm':
                     getAudio(fileNameStr=exampleAudioFileName, translateString=verbExamplesList[i], voiceName='Maxim')
+                    print('getting',verbExamplesList[i],'as',exampleAudioFileName)
                 elif voice == 'f' or voice == '':
                     getAudio(fileNameStr=exampleAudioFileName, translateString=verbExamplesList[i], voiceName='Tatyana')
-                elif voice == 'c':
-                    continue
+                    print('getting',verbExamplesList[i],'as',exampleAudioFileName)
